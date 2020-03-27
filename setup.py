ad117b82a6703e30714d214c55367c21647022f5
Ana <ana@predictiva.io>
2020-03-27 19:14:01 +0100

    Add cookiercutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Introduction to Data Version Control with a a simple natural language processing (NLP) project.',
+    author='agomezandrades',
+    license='MIT',
+)
